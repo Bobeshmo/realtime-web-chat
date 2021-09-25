c661df82a9bdeb4b8bc7bcf99752f967b4213de1
Bobeshmo <bobeshmo@gmail.com>
2021-09-25 09:16:36 +0300

    Log in component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React from 'react';
 import './App.css';
+import {toast} from 'react-toastify'
+import {Switch, Route} from 'react-router-dom'
+import Login from './components/Login/Login'
+import Main from './components/Main/Main'
 
-function App() {
-  return (
-    <div className="App">
+function App () {
 
-    </div>
-  );
+    const showToast = (type, message) => {
+        // 0 = warning, 1 = success
+        switch (type) {
+            case 0:
+                toast.warning(message)
+                break
+            case 1:
+                toast.success(message)
+                break
+            default:
+                break
+        }
+    }
+
+
+    return (
+        <div className="App">
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={props => <Login showToast={showToast} {...props} />}
+                />
+                <Route
+                    exact
+                    path="/main"
+                    render={props => <Main showToast={showToast} {...props} />}
+                />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/base.js
+++ src/base.js
@@ -1,20 +1,14 @@
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/database'
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-};
+});
 
-firebase.initializeApp(firebaseConfig)
-firebase.firestore().settings({
-    timestampsInSnapshots: true
-})
-
-export const myFirebase = firebase
-export const myFirestore = firebase.firestore()
-export const myStorage = firebase.storage()
+export default app;
